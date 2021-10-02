5c373c13432d8f0eee5924d7577656c4c034e4c3
Tomas Badura <xbadurat@stuba.sk>
2021-10-02 17:23:12 +0200

    Revert "feat: fixed find.c memory violation, based on xargs tests"
    This reverts commit c87b3e3add859c8bbd788c36ed3ca0f10f263666.

--- user/find.c
+++ user/find.c
@@ -4,48 +4,12 @@
 #include "kernel/fs.h"
 
 void
-find_rec(char *directory, char *buf, int fd, struct stat st, char *file)
-{
-  char *q;
-  int fd_rec;
-  struct dirent de;
-  struct stat st_rec;
-  
-  if(strlen(directory) + 1 + DIRSIZ + 1 > 512){
-      printf("find: path too long\n");
-      return;
-  }
-  strcpy(buf, directory);
-  q = buf+strlen(buf);
-  *q++ = '/';
-  while(read(fd, &de, sizeof(de)) == sizeof(de)){
-    if(de.inum == 0)
-      continue;
-    memmove(q, de.name, DIRSIZ);
-    q[DIRSIZ] = 0;
-    if(stat(buf, &st) < 0){       
-      printf("find: cannot stat %s\n", buf);
-      continue;
-    }
-    // Check is current file is T_FILE
-    if(st.type == 2 && !strcmp(de.name,file)){
-      printf("%s\n",buf);
-    }  
-    // Check if current file is T_DIR, exclude . and .. directories from recursion
-    if(st.type == 1 && strcmp(de.name,".") && strcmp(de.name,"..")){
-      fd_rec = open(buf, 0);
-      find_rec(buf, buf, fd_rec, st_rec, file);
-      close(fd_rec);
-    }
-  }
-}
-
-void
 find(char *directory, char *file)
 {
-  char buf[512];
+  char buf[512], path[128], *p = path, *q;
   int fd;
   struct stat st;
+  struct dirent de;
 
   if((fd = open(directory, 0)) < 0){
     fprintf(2, "find: cannot open %s\n", directory);
@@ -57,9 +21,48 @@ find(char *directory, char *file)
     close(fd);
     return;
   }
-  // If starting point valid directory, start recursion
-  if(st.type == 1){
-    find_rec(directory, buf, fd, st, file);
+  
+  memmove(p,directory,strlen(directory));
+  p = p+strlen(directory);
+  *p++ = '/';
+  
+  switch(st.type){
+  case T_FILE:
+    printf("find: %s is not a valid directory\n",directory);
+    break;
+    
+  case T_DIR:
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+      printf("find: path too long\n");
+      break;
+    }
+    strcpy(buf, directory);
+    q = buf+strlen(buf);
+    *q++ = '/';
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+      if(de.inum == 0)
+      continue;
+      memmove(q, de.name, DIRSIZ);
+      q[DIRSIZ] = 0;
+      if(stat(buf, &st) < 0){
+        printf("find: cannot stat %s\n", buf);
+        continue;
+      }
+      // Check is current file is T_FILE
+      if(st.type == 2 && !strcmp(de.name,file)){
+        memmove(p,de.name,strlen(de.name));
+        printf("%s\n",path);
+      }
+      
+      // Check if current file is T_DIR
+      if(st.type == 1){
+      	// Exclude . and .. directories from recursion
+        if(strcmp(de.name,".") && strcmp(de.name,"..")){
+	  find(buf,file);
+        }
+      }
+    }
+    break;
   }
   close(fd);
 }
