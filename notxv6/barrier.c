059d32fd4699f3deb8f3ede3a34941875f2b9fe3
Tomas Badura <xbadurat@stuba.sk>
2021-11-10 19:26:04 +0100

    feat: implement barrier

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -25,12 +25,20 @@ barrier_init(void)
 static void 
 barrier()
 {
-  // YOUR CODE HERE
-  //
-  // Block until all threads have called barrier() and
-  // then increment bstate.round.
-  //
-  
+  // Lock before incrementing number of thread that reached barrier
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread++;
+  // When all the threads reach barrier, increment the amount of rounds
+  if(bstate.nthread == nthread) {
+    bstate.round++;
+    // Wake up all the threads, set the amount of threads in barrier to 0
+    pthread_cond_broadcast(&bstate.barrier_cond);
+    bstate.nthread = 0;
+    // If all the threads havent reached the barrier, go to sleep
+  } else {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
